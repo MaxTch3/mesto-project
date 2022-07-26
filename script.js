15bbd26ffa49c3ec165650cc05de68bb5260eb28
Maxim Chalikov <aracctur@yandex.ru>
2022-07-26 22:59:50 +0300

    feature/Добавить окно добавления новых мест со всей функциональностью

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const popUp = document.querySelector('.popup');
 // функция открытия/закрытия PopUp окна
 function togglePopUp() {
   popUp.classList.toggle('popup_opened')
-}
+};
 
 // кнопка Редактирования профиля - вызываем окно PopUp
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -79,3 +79,58 @@ elements.innerHTML = '';
 for (let i = 0; i < elementsList.length; i++) {
   elements.append(elementsList[i]);
 }
+//----------------------------------------------------------
+//Форма добавления карточки
+//----------------------------------------------------------
+//Копируем popUp
+const popUpAddCard = popUp.cloneNode(true);
+
+//Изменяем имя формы
+const formPopUpAddCard = popUpAddCard.querySelector('.popup__form');
+formPopUpAddCard.name = 'add-form';
+//Заменяем заголовок карточки
+const titlePopUpAddCard = popUpAddCard.querySelector('.popup__title');
+titlePopUpAddCard.textContent = 'Новое место';
+//Заменяем классы полей ввода
+const inputPopUp = popUpAddCard.querySelectorAll('.popup__input');
+inputPopUp[0].classList = 'popup__input popup__input_name_image-title';
+inputPopUp[0].name = 'image-name';
+inputPopUp[0].value = '';
+inputPopUp[0].placeholder = 'Название';
+inputPopUp[1].classList = 'popup__input popup__input_name_image-url';
+inputPopUp[1].name = 'image-url';
+inputPopUp[1].value = '';
+inputPopUp[1].placeholder = 'Ссылка на картинку';
+
+// изменяем имя на кнопке
+const addButton = popUpAddCard.querySelector('.popup__submit');
+addButton.textContent = 'Создать';
+// Добавляем форму в разметку
+popUp.after(popUpAddCard);
+
+function togglePopUpAddCard() {
+  popUpAddCard.classList.toggle('popup_opened')
+};
+
+const profileAddButton = document.querySelector('.profile__add-button');
+profileAddButton.addEventListener('click', togglePopUpAddCard);
+
+const popupAddCardCloseButton = popUpAddCard.querySelector('.popup__close-button');
+popupAddCardCloseButton.addEventListener('click', togglePopUpAddCard);
+
+//Добавляем возможность добавлять карточки
+const formAddElement = popUpAddCard.querySelector('.popup__form');
+function formAddSubmitHandler(evt) {
+  evt.preventDefault();
+  const element = elements.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementName = element.querySelector('.element__title');
+  elementImage.src = formAddElement.querySelector('.popup__input_name_image-url').value;
+  elementName.textContent = formAddElement.querySelector('.popup__input_name_image-title').value;
+  elements.prepend(element);
+  formAddElement.querySelector('.popup__input_name_image-url').value = '';
+  formAddElement.querySelector('.popup__input_name_image-title').value = '';
+  togglePopUpAddCard();
+}
+formAddElement.addEventListener('submit', formAddSubmitHandler);
+
