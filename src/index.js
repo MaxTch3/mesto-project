2512f8987db6054bdd3f7a6bad5cfe6a2bcf9884
Maxim Chalikov <aracctur@yandex.ru>
2022-09-25 18:35:56 +0300

    fix: добавлена валидация полей для формы новое место, файл js: исправлено объявление функций

--- src/index.js
+++ src/index.js
@@ -158,23 +158,21 @@ initialCards.forEach((item) => {
   addCard(newCard);
 });
 
-//-------------------------------
-
-const showInputError = (formElement, inputElement, errorMessage) => {
+function showInputError(formElement, inputElement, errorMessage) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input_type_error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('popup__input-error_active');
-};
+}
 
-const hideInputError = (formElement, inputElement) => {
+function hideInputError(formElement, inputElement) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('popup__input_type_error');
   errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
-};
+}
 
-const isValid = (formElement, inputElement) => {
+function isValid(formElement, inputElement) {
   if (inputElement.validity.patternMismatch) {
     inputElement.setCustomValidity("Разрешены латинские буквы, кириллические буквы, знаки дефиса и пробелы ");
   } else {
@@ -186,15 +184,15 @@ const isValid = (formElement, inputElement) => {
   } else {
     hideInputError(formElement, inputElement);
   }
-};
+}
 
-const hasInvalidInput = (inputList) => {
+function hasInvalidInput(inputList) {
   return inputList.some((inputElement) => {
-    return !inputElement.validity.valid
-  })
-};
+    return !inputElement.validity.valid;
+  });
+}
 
-const toggleButtonState = (inputList, buttonElement) => {
+function toggleButtonState(inputList, buttonElement) {
   if (hasInvalidInput(inputList)) {
     buttonElement.disabled = true;
   } else {
@@ -202,7 +200,7 @@ const toggleButtonState = (inputList, buttonElement) => {
   }
 }
 
-const setEventListeners = (formElement) => {
+function setEventListeners(formElement) {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
   const buttonElement = formElement.querySelector('.popup__submit');
   toggleButtonState(inputList, buttonElement);
@@ -212,7 +210,7 @@ const setEventListeners = (formElement) => {
       toggleButtonState(inputList, buttonElement);
     });
   });
-};
+}
 
 function enableValidation() {
   const formList = Array.from(document.querySelectorAll('.popup__form'));
